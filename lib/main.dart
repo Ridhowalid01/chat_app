747ff6532764c419c24b5f25d111b90a04e72e09
Ridhowalid01 <rialdifreedom01@gmail.com>
2025-11-28 09:47:16 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+    );
+  }
+}
