41a82aee2044d92c0045e4bda99465a92e2800eb
Ridhowalid01 <rialdifreedom01@gmail.com>
2025-11-30 14:03:20 +0700

    integrated with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:chat_app/ui/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +20,10 @@ class MyApp extends StatelessWidget {
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 63, 17, 177),
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
+      home: const AuthScreen(),
     );
   }
 }
